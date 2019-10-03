5df5dadf91fb97674ede25033e32a91caf09196d
MingLin529 <51132890+MingLin529@users.noreply.github.com>
2019-10-03 08:49:08 -0600

    Create pseudocode.py

---
+++ pseudocode.py
@@ -0,0 +1,96 @@
+# Press E to print out inventory
+
+# After a plot, given two or more opitions, choose the right way can lead
+# to endings, include good endings, normal endings, and ture
+# endings or leading to another plot with opitions. Choose the wrong way can
+# lead to bad endings which will game over. Some important opitions will
+# effect the endings some does not. Each opition will effect the plot.
+# The game will only come to an end after few opitions, whether its good or
+# bad opition, just one opition will not leading to an end.
+
+# To have the True Ending, you have to choose the following opitions in order:
+# 1-search for wifi
+# 2-turn off the phone to save battery
+# 3-take the black hair with you
+# 4-lock the door
+# 5-flip through the window to next door
+# 6-(opition does not matter, both works)
+# 7-hide behind the door
+# 8-investigate the vase
+# 9-directly open the lock
+# 10-leave after investigate
+
+# End 1-Is it a dream?:
+# 1-search for wifi
+# 2-(both works)
+# 3-leave the hair
+# 4-lay back to bed
+
+# End 2-You died(You pretended asleep after you took something? Wake up!):
+# 1-search for wifi
+# 2-(both works)
+# 3-take the black hair with you
+# 4-lay back to bed
+
+# End 3-You died(Sounds too loud!):
+# 1-watch YouTube
+
+# End 4-You died(Found by dog):
+# 1-search for wifi
+# 2-(both works)
+# 3-(both works)
+# 4-lock the door
+# 5-jump down the window
+# 6-go left
+
+# End 5-You died(Tripped and dropped your phone. Trapped on boat!):
+# 1-search for wifi
+# 2-(both works)
+# 3-(both works)
+# 4-lock the door
+# 5-jump down the window
+# 6-go right
+# 7-find boat
+
+# End 6-You died(Can you really wait until the police comes?):
+# 1-search for wifi
+# 2-(both works)
+# 3-(both works)
+# 4-lock the door
+# 5-jump down the window
+# 6-go right
+# 7-take out phone
+# 8-call 911
+
+# End 7-You died(Mom phones you. You were found!):
+# 1-search for wifi
+# 2-(both works)
+# 3-(both works)
+# 4-lock the door
+# 5-jump down the window
+# 6-go right
+# 7-take out phone
+# 8-text message to mom
+
+# End 8-You died(You were found by the dog downstairs.):
+# 1-search for wifi
+# 2-(both works)
+# 3-(both works)
+# 4-lock the door
+# 5-flip through the window to next door
+# 6-(both works)
+# 7-jump down the window
+
+# End 9-You died(The phone is dead!):
+# 1-search for wifi
+# 2-turn on low battery mode to save battery
+# 3-(both works)
+# 4-lock the door
+# 5-flip through the window to next door
+# 6-(both works)
+# 7-hide behind the door
+# 8-investigate the vase
+# 9-directly open the lock
+# 10-leave after investigate
+
+# End 10-Success
