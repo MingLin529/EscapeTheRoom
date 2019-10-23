8cdd74d6651a52dd770b97a29b9237503f99d7b9
MingLin529 <51132890+MingLin529@users.noreply.github.com>
2019-10-23 09:19:29 -0600

    Update game.py

--- game.py
+++ game.py
@@ -31,60 +31,83 @@ for tool in tool_gain:
 
 
 # character and traits
-character = {
-'John': {
-'First name': 'John',
-'Last name': 'Anderson',
-'Identity': 'College Student',
-},
-}
+characters = {
+    'John': {
+        'First name': 'John',
+        'Last name': 'Anderson',
+        'Identity': 'College Student',
+        },
+    }
 
 # locations and descriptions
 
 locations = {
-'Room 1': {
-'Description': 'It is the guest room and the starting room',
-},
-'Room 2': {
-'Decription': 'It is the master room',
-},
-'Room 3': {
-'Decription': 'It is the living room',
-},
-'Room 4': {
-'Decription': 'It is the basement',
-},
-}
+    'Room 1': {
+        'Description': 'It is the guest room and the starting room',
+        },
+    'Room 2': {
+        'Decription': 'It is the master room',
+        },
+    'Room 3': {
+        'Decription': 'It is the living room',
+        },
+    'Room 4': {
+        'Decription': 'It is the basement',
+        },
+    }
 
 # inventory and characteristics
 
 item_have = {
-'Phone': {
-'Description': 'This is the only item you have when you woke up',
-'Condition': 'With only 2% battery and no WIFI signal',
-'Usage': 'You can use to contact and watch YouTube if you find WIFI',
-},
-}
+    'Phone': {
+        'Description': 'This is the only item you have when you woke up',
+        'Condition': 'With only 2% battery and no WIFI signal',
+        'Usage': 'You can use to contact and watch YouTube if you find WIFI',
+        },
+    }
 
-tool_gain = {
-'Cross key': {
-'Description': '', 
-'Condition': '',
-'Usage': '',
-},
-'Journal': {
-'Description': '',
-'Condition': '',
-'Usage': '',
-},
-'Gold key': {
-'Description': '',
-'Condition': '',
-'Usage': '',
-},
-'Paper strip': {
-'Description': '',
-'Condition': '',
-'Usage': '',
-},
-}
+tool_gain = []
+
+tool_1 = {
+    'Cross key': {
+        'Description': 'This is a key shaped as a cross',
+        'Condition': 'Other than the shape, there is nothing special '
+        'about the key',
+        'Usage': 'It can only open the specific lock',
+        },
+    }
+
+tool_2 = {
+    'Journal': {
+        'Description': 'This is the journal from some else',
+        'Condition': 'The trace left on the journal showed that '
+        'the owner have written on it everyday',
+        'Usage': 'Maybe you can learn some information about the situation '
+        'you are involved in',
+        },
+    }
+
+tool_3 = {
+    'Gold key': {
+        'Description': 'This is a gold key',
+        'Condition': 'It is just a key that is colored in gold, not that '
+        'the material is gold',
+        'Usage': 'You can use to open a lock, it is not used for you to '
+        'sell it for money',
+        },
+    }
+
+tool_4 = {
+    'Paper strip': {
+        'Description': 'This is a strip made out of paper',
+        'Condition': 'It belonged to a notebook, you can easily come up '
+        'with the solution by the rough edge of the strip',
+        'Usage': 'Some werid notes has written on it, maybe it is the key'
+        'to a mistery or maybe it is just some sketch',
+        },
+    }
+
+tool_gain.append(tool_1)
+tool_gain.insert(1, tool_2)
+tool_gain.insert(2, tool_3)
+tool_gain.insert(3, tool_4)
