fbf27ec381d09ff6f31cf1ca76d842c71d4f6546
MingLin529 <51132890+MingLin529@users.noreply.github.com>
2019-10-30 08:39:55 -0600

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,29 @@
+# Course: CS 30
+# Period: 1
+# Date created: 19/10/29
+# Date last modified: 19/10/30
+# Name: Mingyuan Lin
+# Description: a simple menu for the player to follow and make choices
+
+
+# the choice of directions you have
+
+directions = ['flip through the window to next door',
+             'jump down the window',
+             'head straight downstairs']
+
+# print out the question and choices
+
+print("Which direction are you taking? (print out the number)")
+for direction in directions:
+  print(f"{directions.index(direction)+1}. {direction}")
+
+# print out a specific choice when you entered an input
+
+name = int(input("Enter choice: "))
+if name == 1:
+  print("flip through the window to next door")
+elif name == 2:
+  print("jump down the window")
+else:
+  print("head straight downstairs")
