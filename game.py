7cdde235cbc094293086cbe3a2b86655a9a878d9
MingLin529 <51132890+MingLin529@users.noreply.github.com>
2019-11-15 08:40:56 -0600

    Update game.py

--- game.py
+++ game.py
@@ -110,3 +110,40 @@ for tool, tool_info in tool_gain.items():
     print(f"* {tool}:")
     for sub, des in tool_info.items():
         print(f"\t{sub}: {des}.")
+
+
+# the choice of directions you have
+
+directions = ['flip through the window to next door',
+              'jump down the window',
+              'head straight downstairs']
+
+# Instructions to follow
+
+prompt = "\nEnter a number in front of the directions you want to go: "
+prompt += "\n(Enter 0 when you are stopping.)"
+
+
+def actions():
+    """loop out the choise from dictionary with its index"""
+    for direction in directions:
+        print(f"{directions.index(direction)+1}. {direction}")
+
+
+# keep looping while the input is not 0. When its 0, the loop quit
+
+while True:
+    actions()
+
+    name = int(input(f"{prompt}\n"))
+
+    if name == 0:
+        break
+    elif name == 1:
+        print(f"\nYou {directions[0]}.\n")
+    elif name == 2:
+        print(f"\nYou {directions[1]}.\n")
+    elif name == 3:
+        print(f"\nYou {directions[2]}.\n")
+    else:
+        print("\nThis number is INVALID. Please choose another number.\n")
