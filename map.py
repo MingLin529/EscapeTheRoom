cf6695c266f108e27566e8a397ff2f9eeb381753
MingLin529 <51132890+MingLin529@users.noreply.github.com>
2019-11-20 09:27:05 -0600

    Create map.py

---
+++ map.py
@@ -0,0 +1,46 @@
+# Course: CS 30
+# Period: 1
+# Date created: 19/11/15
+# Date last modified:
+# Name: Mingyuan Lin
+# Description: prints out a map for the place where you are
+
+# Locations and descriptions
+
+# These are the rooms you are going to go throw in the game
+
+# locations = {
+#    'Room 1': {
+#        'Description': 'is the guest room and the starting room',
+#        },
+#    'Room 2': {
+#        'Description': 'is the master room',
+#        },
+#    'Room 3': {
+#        'Description': 'is the living room',
+#        },
+#    'Room 4': {
+#        'Description': 'is the basement',
+#        },
+#    }
+
+# print("\nThese are the locations you are going to investe in the house:")
+# for room, room_info in locations.items():
+#    description = room_info['Description']
+
+#    print(f"\t{room} {description}.")
+
+
+# defining the layout of the house
+house_map = [
+    ['Guest Room (Start)', 'Living Room'],
+    ['Master Room', 'Living Room'],
+    ['Basement', 'Basement']
+]
+
+
+def print_map():
+    """print out the map in array"""
+    print("This is the layout of the house you are in: ")
+    for map in house_map:
+        print(map)
